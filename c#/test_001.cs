dc5721e8f9d6d6b3614aa210b9def2804c69cafc
Marcuth <thiago.playgames15@gmail.com>
2022-11-28 23:37:38 +0000

    tests

---
+++ c#/test_001.cs
@@ -0,0 +1,10 @@
+namespace HelloWorldProgram
+{
+    class HelloWorld
+    {
+        static void Main() {
+            Console.WriteLine("Hello World");
+            Console.ReadKey(); // Espera alguma tecla ser precionada para sair do programa
+        }
+    }
+}
